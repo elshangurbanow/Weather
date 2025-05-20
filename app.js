bcd7ac65f08a0387aed3bf897223be867307e624
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-20 18:24:32 +0000

    chance

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-"use strict";
 $(document).ready(function () {
+
+
+
+
+
+$(document).on('keypress', function (e) {
+    if (e.which == 13) {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&unitsmetric`
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels.like)
+            $("myWeather").val("")
+            console.log(data)
+        }
+        myweather()
+    }
+})
+
+
+
 
 });
