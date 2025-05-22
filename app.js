68e92f5bc5b1419b1547436155d5595f4337bac8
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-22 19:57:23 +0000

    commited

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ $(document).ready(function () {
 
 
 
-$(document).on('keypress', function (e) {
-    if (e.which == 13) {
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&unitsmetric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels.like)
+                $("myWeather").val("")
+                console.log(data)
+            }
+            myweather()
+        }
+    })
+
+
+    $("#btn").click(function (e) {
+        e.preventDefault()
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&unitsmetric`
         async function myweather() {
             let responce = await fetch(url)
@@ -17,10 +34,8 @@ $(document).on('keypress', function (e) {
             console.log(data)
         }
         myweather()
-    }
-})
 
-
+    })
 
 
 });
