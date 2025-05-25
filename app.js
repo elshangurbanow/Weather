5b8e86e3d5b516c65eb6b8b8658fea1c99ac3143
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-25 14:53:08 +0000

    commited

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&unitsmetric`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
                 $("#city").html(data.name)
                 $("#temp").html(data.main.temp)
-                $("#feels").html(data.main.feels.like)
+                $("#feels").html(data.main.feels_like)
                 $("myWeather").val("")
                 console.log(data)
             }
@@ -23,19 +23,24 @@ $(document).ready(function () {
 
     $("#btn").click(function (e) {
         e.preventDefault()
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&unitsmetric`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
         async function myweather() {
             let responce = await fetch(url)
             let data = await responce.json()
             $("#city").html(data.name)
             $("#temp").html(data.main.temp)
-            $("#feels").html(data.main.feels.like)
+            $("#feels").html(data.main.feels_like)
             $("myWeather").val("")
             console.log(data)
         }
         myweather()
 
     })
+
+
+
+
+
 
 
 });
